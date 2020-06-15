005651faf1b00f174f210ba2bf327d1dfe6610a5
mpetavy <mpetavy>
2020-06-15 23:06:30 +0200

    common v1.1.36

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ var (
 )
 
 func init() {
-	common.Init("1.0.3", "2017", "Tool to inspect DICOM file header and content", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
+	common.Init(false, "1.0.3", "2017", "Tool to inspect DICOM file header and content", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
 }
 
 func find(l []string, e string) bool {
